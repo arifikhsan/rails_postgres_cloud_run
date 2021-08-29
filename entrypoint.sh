7c83b7b0d6b03796ff95885a945aa0085f0633e9
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-08-29 17:41:18 +0700

    migrate

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e
 
+pwd
+
+# Create the Rails production DB on first run
+bundle exec rake db:create
+
+# Make sure we are using the most up to date
+# database schema
+bundle exec rake db:migrate
+
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
 
